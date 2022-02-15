b2d878029e0b2a70524b978d001016284dedb4fc
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2022-02-15 10:29:40 +0100

    Updates

---
+++ precache-manifest.26a2e146e24be67d1b52da8ebf6b0534.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc8d056ccdbfa5d12c4527ec947703b",
+    "url": "/ebsi-wallet-wev-client/index.html"
+  },
+  {
+    "revision": "db4c2c07dd5eca6c5730",
+    "url": "/ebsi-wallet-wev-client/static/css/2.a756f9d1.chunk.css"
+  },
+  {
+    "revision": "45561debf766f8d17537",
+    "url": "/ebsi-wallet-wev-client/static/css/main.3da598ba.chunk.css"
+  },
+  {
+    "revision": "db4c2c07dd5eca6c5730",
+    "url": "/ebsi-wallet-wev-client/static/js/2.d906a194.chunk.js"
+  },
+  {
+    "revision": "9b008ba347f629f2368d610a9f4c7d19",
+    "url": "/ebsi-wallet-wev-client/static/js/2.d906a194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45561debf766f8d17537",
+    "url": "/ebsi-wallet-wev-client/static/js/main.600d55d2.chunk.js"
+  },
+  {
+    "revision": "a6ecb52ea1cf2894bef9",
+    "url": "/ebsi-wallet-wev-client/static/js/runtime-main.5dd6a5ca.js"
+  },
+  {
+    "revision": "1c321bf57ebe86ac5c40cedcc177fe73",
+    "url": "/ebsi-wallet-wev-client/static/media/eu-background.1c321bf5.svg"
+  },
+  {
+    "revision": "38159e270af7cb4684d19cddb62df534",
+    "url": "/ebsi-wallet-wev-client/static/media/european-union.38159e27.png"
+  },
+  {
+    "revision": "965468b2c74f533057f6211c243a7ba0",
+    "url": "/ebsi-wallet-wev-client/static/media/icons.965468b2.svg"
+  },
+  {
+    "revision": "30b933ccc0f2663b1c3b73db1884926c",
+    "url": "/ebsi-wallet-wev-client/static/media/logo--en.30b933cc.svg"
+  },
+  {
+    "revision": "8ea11e8dc5ed4e7b97414142192ef586",
+    "url": "/ebsi-wallet-wev-client/static/media/step1.8ea11e8d.svg"
+  }
+]);
