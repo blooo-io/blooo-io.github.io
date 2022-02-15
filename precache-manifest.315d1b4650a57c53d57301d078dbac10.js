e3c4ac74c2eb7f84f2202d16a830062c02a1bf92
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2022-02-15 11:40:04 +0100

    Updates

---
+++ precache-manifest.315d1b4650a57c53d57301d078dbac10.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec835ba1748728262b1c3fbc83541f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae233fbbf1ae375e64a9",
+    "url": "/static/css/2.71133d1b.chunk.css"
+  },
+  {
+    "revision": "4cc19f91e4c4b0638b67",
+    "url": "/static/css/main.f574c968.chunk.css"
+  },
+  {
+    "revision": "ae233fbbf1ae375e64a9",
+    "url": "/static/js/2.ff26b322.chunk.js"
+  },
+  {
+    "revision": "9b008ba347f629f2368d610a9f4c7d19",
+    "url": "/static/js/2.ff26b322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc19f91e4c4b0638b67",
+    "url": "/static/js/main.6df06f3c.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "1c321bf57ebe86ac5c40cedcc177fe73",
+    "url": "/static/media/eu-background.1c321bf5.svg"
+  },
+  {
+    "revision": "38159e270af7cb4684d19cddb62df534",
+    "url": "/static/media/european-union.38159e27.png"
+  },
+  {
+    "revision": "965468b2c74f533057f6211c243a7ba0",
+    "url": "/static/media/icons.965468b2.svg"
+  },
+  {
+    "revision": "30b933ccc0f2663b1c3b73db1884926c",
+    "url": "/static/media/logo--en.30b933cc.svg"
+  },
+  {
+    "revision": "8ea11e8dc5ed4e7b97414142192ef586",
+    "url": "/static/media/step1.8ea11e8d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.897f0ae033245346ceebe0423c3d1ca0.js"
+  "/precache-manifest.315d1b4650a57c53d57301d078dbac10.js"
 );
 
 self.addEventListener('message', (event) => {
